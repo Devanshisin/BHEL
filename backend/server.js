190ec0fbc56f6d7f2b4aaca43f5a579696cad471
Devanshi Singh <166386439+Devanshisin@users.noreply.github.com>
2026-04-08 19:04:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/uploads', express.static('uploads'));
