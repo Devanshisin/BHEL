b60343790d9bc566b3d0bfcfbb5c25983616c5e1
Devanshi Singh <166386439+Devanshisin@users.noreply.github.com>
2026-04-08 17:56:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database');
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config();;
 
 const app = express();
 
